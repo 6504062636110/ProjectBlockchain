2adbe335fdcc0a93f6f6597257fffbdd0e8805df
KRXTON MAC <krxtonmac@KRXTON-MACBOOK.local>
2025-02-08 00:04:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="fonthead">
+          TIGGER-B
+        </h1>
+        <input 
+          type="text" 
+          placeholder="กรอกข้อความที่นี่..." 
+          className="input-box"
+        />
+      </div>
+    </>
+  )
+}
+
+export default App

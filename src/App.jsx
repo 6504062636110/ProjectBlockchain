356eb54f4db82d9b0463453c19114a1f03132d63
6504062636110 <s6504062636110@email.kmutnb.ac.th>
2025-02-08 15:13:30 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="container">
-        <h1 className="fonthead">
-          TIGGER-B
-        </h1>
+        <h1 className="fonthead">TIGGER-B</h1>
         <input 
           type="text" 
           placeholder="กรอกข้อความที่นี่..." 
           className="input-box"
         />
+        <button className="btn" onClick={() => alert("คุณกดปุ่ม!")}>
+          กดที่นี่
+        </button>
       </div>
     </>
-  )
+  );
 }
 
 export default App
+
